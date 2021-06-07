0c1d9fc7ffdd3546f41dda582809d9639654f844
Elias Babaeff <52626664+nippach@users.noreply.github.com>
2021-06-07 19:39:09 +0300

    Revert "Add TimestampFormatter"
    This reverts commit 7c627ff3280d9b4a147367018c3633a4fafcbc5c.

---
+++ Frameworks/TimestampFormatter.framework/Headers/TimestampFormatter.h
@@ -0,0 +1,18 @@
+//
+//  TimestampFormatter.h
+//  TimestampFormatter
+//
+//  Created by Admin on 30.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TimestampFormatter.
+FOUNDATION_EXPORT double TimestampFormatterVersionNumber;
+
+//! Project version string for TimestampFormatter.
+FOUNDATION_EXPORT const unsigned char TimestampFormatterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimestampFormatter/PublicHeader.h>
+
+
